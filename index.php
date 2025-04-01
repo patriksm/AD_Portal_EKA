2f084019c9b9e1ca2733e4d77bb5c12615f62b23
Patric <pmorevs@gmail.com>
2025-04-01 10:28:12 +0300

    DB training

--- index.php
+++ index.php
@@ -1,52 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- <meta charset="UTF-8"> -->
-    <meta charset="utf8mb4">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ADS Latvia</title>
-    <link rel="stylesheet" href="css\style.css">
+    <title>DB conn w3Schools</title>
 </head>
 <body>
-    <?php
-        $text = "How let us connect to DataBase";
-    ?>
-    <div>
-        <b>Hello world! <?php echo $text ?></b>
-        <br>
-        <b>
-            <?php
-                $dbcon=mysqli_connect("localhost:3306", "root", "usbw", "test1");
-                if($dbcon == false){
-                    print("Connection to DB is not established! " . mysqli_connect_error());
-                } else {
-                    print("Connection Successful! <br>");
-                    $sql = 'SELECT id, name FROM cities';
-                    $result = mysqli_query($dbcon, $sql);
-                    
-                    if($result == false){
-                        print("There is an error with connection to the database!".mysqli_connect_error());
-                    } else {
-                        while($row = mysqli_fetch_array($result)){
-                            print("Id = " . $row['id'] . "; City Name = " . $row['name'] . "<br>");
-                        }
-                    }
-                }
-            ?>
-        </b>
-    </div>
-    <div>
-        <p>F O R M</p>
-        <form method="POST">
-            <label>My Message</label>
-            <input type="text" id="my_mes" name="mymes"></input>
-            <input type="submit" id="my_sbmt" name="mysbmt"></input>
-        </form>
-    </div>
-
-    <?php 
-        $mytext = $_POST['mymes'];
-        echo "<p>".$mytext."</p>";
-    ?>
+    
 </body>
 </html>
